7e399bc1937a8ec8b499f7b4dd9bd7c068dfb6cb
Hector <hector.montes1@upr.edu>
2018-02-25 12:12:31 -0400

    Revert "Added Perimeter Feature"
    This reverts commit 1483b3cbf9f45d4ecde7c7daa21425a4f466015a.

--- Main.java
+++ Main.java
@@ -5,22 +5,14 @@ public class Main {
 	public static void main(String[] args) {
 		
 		float area;
-		float halfArea;
-		float perimeter;
 		double radius;
-		
 		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter radius of circle: ");
-		
 		if(sc.hasNextDouble()) {
 			radius = sc.nextDouble();			
 			area = (float) (Math.pow(radius, 2) * Math.PI);
-			halfArea = area / 2;
-			perimeter = (float) (2 * Math.PI * radius);
-			System.out.printf("The approximate area of the circle is: %.3f \n", area);
-			System.out.printf("The half area of the circle is: %.3f \n", halfArea);
-			System.out.printf("The perimeter of the circle is: %.3f", perimeter);
+			System.out.printf("The approximate area of the circle is: %.4f", area);
 		}
 		else {
 			System.out.println("Invalid input entered.");
